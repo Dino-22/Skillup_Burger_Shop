2f5fc850a4f8bb5755187614953b26c620792094
Dean Watson <deanwatson@live.co.uk>
2023-12-01 02:58:31 +0000

    Add About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,1 +1,35 @@
 // Write all the code here
+import React from "react";
+import Founder from "../home/Founder";
+import me from "../../assets/skj.jpg";
+import { LiaSearchengin } from "react-icons/lia";
+
+function About() {
+    return(
+        <section className="about">
+            <main className="about">
+                <h1>About Us</h1>
+                <article>
+                    <h4>Burger Shop</h4>
+                    <p>This is Burger Shop. The place ofr most tasty burges on the entire earth.</p>
+                    <p>Explore the various types of food and burgers. Click below to see the menu</p>
+                    <a href="home"><LiaSearchengin /></a>
+                </article>
+                <div>
+                    <h2>Founder</h2>
+                    <article>
+                        <div>
+                            <img src={me} alt="Founder" height={200} width={200} />
+                            <h3>Nelson</h3>
+                        </div>
+                        <p>Hey, Everyone I am Nelson, the founder of Burger Shop.</p>
+                    </article>
+                    
+                </div>
+            </main>
+            
+        </section>
+    );
+}
+
+export default About;
